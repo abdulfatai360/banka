8f7e9e9f86fcfd64a89a1cf9af71ce5e7b66672f
Abdulfatai Jimoh <abdulfatai360@gmail.com>
2019-04-08 16:53:13 +0100

    [Feature #165051626] Create credit transaction endpoint API

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -6,6 +6,6 @@ import transaction from '../controllers/transaction';
 const router = express.Router();
 
 router.post('/:accountNumber/debit', validateInputs('postTransaction'), transaction.debitAccount);
-// router.post('/:accountNumber/credit', validateInputs('postTransaction'), transaction.creditAccount);
+router.post('/:accountNumber/credit', validateInputs('postTransaction'), transaction.creditAccount);
 
 export default router;
