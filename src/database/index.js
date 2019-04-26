ad03bb39587110a88a80ecf9656a85fb2f5295af
Abdulfatai Jimoh <abdulfatai360@gmail.com>
2019-04-26 13:04:55 +0100

    fix(heroku): fix databse issue

--- src/database/index.js
+++ src/database/index.js
@@ -4,24 +4,32 @@ import winston from 'winston';
 
 dotenv.config();
 
-const databaseConnectionInfo = {
-  development: {
-    host: process.env.PG_HOST || 'localhost',
-    port: process.env.PG_PORT || 5432,
-    database: (process.env.ENV_TEST) ? process.env.PG_DATABASE_TEST : process.env.PG_DATABASE,
-    user: process.env.PG_USER || 'postgres',
-    password: process.env.PG_PASSWORD || null,
-  },
-  // heroku: {
-  //   connectionString: process.env.DATABASE_URL,
-  //   ssl: true,
-  // },
-};
+// const databaseConnectionInfo = {
+//   development: {
+//     host: process.env.PG_HOST || 'localhost',
+//     port: process.env.PG_PORT || 5432,
+//     database: (process.env.ENV_TEST) ? process.env.PG_DATABASE_TEST : process.env.PG_DATABASE,
+//     user: process.env.PG_USER || 'postgres',
+//     password: process.env.PG_PASSWORD || null,
+//   },
+//   heroku: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: true,
+//   },
+// };
 
 // const env = (process.env.ENV === 'heroku') ? 'heroku' : 'development';
-const pool = new Pool(databaseConnectionInfo.development);
+let connectionString;
 
-winston.info(`Connected database: ${databaseConnectionInfo.development.database}`);
+if (process.env.ENV_TEST) {
+  connectionString = process.env.DATABASE_URL_TEST;
+} else {
+  connectionString = process.env.DATABASE_URL;
+}
+
+const pool = new Pool({ connectionString });
+
+winston.info(`Connected database: ${connectionString}`);
 
 export default {
   query(text, values) {
