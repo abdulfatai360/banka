3345740d701f21bc3650c9966e97de28b352d80a
Abdulfatai Jimoh <abdulfatai360@gmail.com>
2019-05-20 23:43:03 +0100

    feature(admin): implement feature to delete and change account status
    [Delivers #166104221]

--- ui/js/bank-accounts.js
+++ ui/js/bank-accounts.js
@@ -1,31 +1,51 @@
 /* ******** global variables ******** */
-const accountsListTemplate = document.querySelector('#bank-accts-ls-template').textContent;
+const accountsListTemplate = document.querySelector('#bank-accts-ls-template')
+  .textContent;
 
 /* ******** generates accounts list from the template ******** */
 const generateAccountsListHtml = (template, accountEntity) => {
   let accountTemplate = template;
 
   let createdOn = new Date(accountEntity.createdOn);
-  createdOn = new Intl.DateTimeFormat('en-US', { day: 'numeric', month: 'numeric', year: 'numeric' }).format(createdOn);
+  createdOn = new Intl.DateTimeFormat('en-US', {
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric'
+  }).format(createdOn);
 
   accountTemplate = accountTemplate.replace('{{createdOn}}', createdOn);
-  accountTemplate = accountTemplate.replace('{{accountNumber}}', accountEntity.accountNumber);
-  accountTemplate = accountTemplate.replace('{{type}}', accountEntity.accountType);
-  accountTemplate = accountTemplate.replace('{{status}}', accountEntity.accountStatus);
-  accountTemplate = accountTemplate.replace('{{openingBalance}}', accountEntity.openingBalance);
-  accountTemplate = accountTemplate.replace('{{balance}}', accountEntity.balance);
+  accountTemplate = accountTemplate.replace(
+    '{{accountNumber}}',
+    accountEntity.accountNumber
+  );
+  accountTemplate = accountTemplate.replace(
+    '{{type}}',
+    accountEntity.accountType
+  );
+  accountTemplate = accountTemplate.replace(
+    '{{status}}',
+    accountEntity.accountStatus
+  );
+  accountTemplate = accountTemplate.replace(
+    '{{openingBalance}}',
+    accountEntity.openingBalance
+  );
+  accountTemplate = accountTemplate.replace(
+    '{{balance}}',
+    accountEntity.balance
+  );
 
   return accountTemplate;
 };
 
 /* ******** render accounts list on the browser ******** */
-const renderAccountsList = (accounts) => {
+const renderAccountsList = accounts => {
   const accountsListElem = document.querySelector('.bank-accts-ls tbody');
   let accountsListHtml = '';
 
   accounts.forEach(account => {
     accountsListHtml += generateAccountsListHtml(accountsListTemplate, account);
-  })
+  });
 
   accountsListElem.innerHTML = accountsListHtml;
 };
@@ -41,6 +61,10 @@ const disableActionButtons = () => {
 
     if (/^dormant$/i.test(accountStatusElem.textContent)) {
       accountStatusElem.parentElement.classList.add('dormant-account');
+    }
+
+    if (/^draft$/i.test(accountStatusElem.textContent)) {
+      accountStatusElem.parentElement.classList.add('draft-account');
     }
   });
 };
@@ -59,6 +83,9 @@ const modalDisplayInit = () => {
 
   modalClosers.forEach(modalCloser => {
     modalCloser.addEventListener('click', () => {
+      const yesButton = document.querySelector('.btn-modal-yes');
+      yesButton.removeEventListener('click', deleteAccount);
+      yesButton.removeEventListener('click', activateOrDeactivateAccount);
       bodyElem.classList.add('modal--hidden');
     });
   });
@@ -66,7 +93,9 @@ const modalDisplayInit = () => {
 
 /* ******** fetch all accounts records ******** */
 const getAllAccounts = () => {
-  const accountsListInnerContainer = document.querySelector('.bank-accts-ls__inner');
+  const accountsListInnerContainer = document.querySelector(
+    '.bank-accts-ls__inner'
+  );
   const accountsListElem = document.querySelector('.bank-accts-ls tbody');
   const token = JSON.parse(localStorage.getItem('token'));
 
@@ -74,15 +103,15 @@ const getAllAccounts = () => {
   const url = 'https://ile-ifowopamo.herokuapp.com/api/v1/accounts';
   const init = {
     headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      "x-auth-token": token
+      'Content-Type': 'application/json; charset=utf-8',
+      'x-auth-token': token
     },
     method: 'GET'
   };
 
   fetch(url, init)
     .then(response => response.json())
-    .then((response) => {
+    .then(response => {
       accountsListInnerContainer.classList.remove('state--loading');
       accountsListInnerContainer.classList.add('state--no-data');
       accountsListElem.textContent = '';
@@ -90,25 +119,28 @@ const getAllAccounts = () => {
       if (response.status !== 200) {
         const accountsListElemSpan = document.createElement('span');
         accountsListElemSpan.textContent = response.error;
-        document.querySelector('.http-message')
+        document
+          .querySelector('.http-message')
           .appendChild(accountsListElemSpan);
-        return false;
+        return null;
       }
 
       if (response.message) {
         const accountsListElemSpan = document.createElement('span');
         accountsListElemSpan.textContent = response.error;
-        document.querySelector('.http-message')
+        document
+          .querySelector('.http-message')
           .appendChild(accountsListElemSpan);
-        return true;
+        return null;
       }
 
       if (!response.data.length) {
         const accountsListElemSpan = document.createElement('span');
         accountsListElemSpan.textContent = 'No accounts in the database';
-        document.querySelector('.http-message')
+        document
+          .querySelector('.http-message')
           .appendChild(accountsListElemSpan);
-        return true;
+        return null;
       }
 
       accountsListInnerContainer.classList.remove('state--no-data');
@@ -122,8 +154,10 @@ const getAllAccounts = () => {
 getAllAccounts();
 
 /* ******** show network message to client ******** */
-const successMessageTemplate = document.querySelector('#success-popup-template').textContent;
-const failureMessageTemplate = document.querySelector('#failure-popup-template').textContent;
+const successMessageTemplate = document.querySelector('#success-popup-template')
+  .textContent;
+const failureMessageTemplate = document.querySelector('#failure-popup-template')
+  .textContent;
 
 const showMessage = (message, messageType) => {
   const yesButton = document.querySelector('.btn-modal-yes');
@@ -134,64 +168,43 @@ const showMessage = (message, messageType) => {
     const messageTemplate = successMessageTemplate;
     htmlString = messageTemplate.replace('{{message}}', message);
   } else {
-    const messageTemplate = errorMessageTemplate;
+    const messageTemplate = failureMessageTemplate;
     htmlString = messageTemplate.replace('{{message}}', message);
   }
 
   popupElem.textContent = '';
   popupElem.innerHTML = htmlString;
   popupElem.parentElement.classList.add('popup--visible');
+
   setTimeout(() => {
     popupElem.parentElement.classList.remove('popup--visible');
     yesButton.classList.remove('btn__loading-icon-visible');
   }, 3000);
 };
 
+/* ******** set account number value ******** */
+let accountNumber;
 
-/* ******** delete a specific account ******** */
-const deleteAccount = (accountNumber) => {
-  const yesButton = document.querySelector('.btn-modal-yes');
+const setAccountNumber = event => {
+  const targetClasslist = event.target.classList;
 
-  const performAction = () => {
-    yesButton.classList.add('btn__loading-icon-visible');
-    const token = JSON.parse(localStorage.getItem('token'));
+  if (
+    targetClasslist.contains('btn-table') ||
+    targetClasslist.contains('icon-tbl')
+  ) {
+    let actionButtonElemCell;
 
-    // const url = `http://localhost:3000/api/v1/accounts/${accountNumber}`;
-    const url = `https://ile-ifowopamo.herokuapp.com/api/v1/accounts/${accountNumber}`;
+    if (
+      targetClasslist.contains('btn-table') ||
+      targetClasslist.contains('icon-table')
+    ) {
+      actionButtonElemCell = event.target.parentElement;
+    }
 
-    const init = {
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        "x-auth-token": token
-      },
-      method: 'DELETE'
-    };
+    if (targetClasslist.contains('icon-tbl--i')) {
+      actionButtonElemCell = event.target.parentElement.parentElement;
+    }
 
-    fetch(url, init)
-      .then(response => response.json())
-      .then(response => {
-        if (response.status === 200) {
-          showMessage(response.message, 'success');
-          document.body.classList.add('modal--hidden');
-
-          getAllAccounts();
-          yesButton.removeEventListener('click', performAction);
-        }
-      })
-      .catch(error => console.log(error.message));
-  };
-
-  yesButton.addEventListener('click', performAction);
-};
-
-/* ******** get account number value and perform due action ******** */
-const getAccountNumberAndPerformAction = (event) => {
-  if (event.target.classList.contains('btn-table')) {
-    let accountNumber;
-    let actionToPerform;
-    const modalMessageElem = document.querySelector('.modal-message');
-
-    const actionButtonElemCell = event.target.parentElement;
     const accountEntityElem = actionButtonElemCell.parentElement;
     const accountAttributesElems = accountEntityElem.children;
 
@@ -201,14 +214,125 @@ const getAccountNumberAndPerformAction = (event) => {
         accountNumber = accountAttributeElem.textContent;
       }
     }
+  }
+};
 
-    if (event.target.classList.contains('btn-delete')) {
-      actionToPerform = 'delete';
-      modalMessageElem.textContent = `Are you sure you want to ${actionToPerform} this '${accountNumber}' account?`
-      deleteAccount(accountNumber);
+/* ******** delete a specific account ******** */
+const deleteAccount = () => {
+  const yesButton = document.querySelector('.btn-modal-yes');
+
+  yesButton.classList.add('btn__loading-icon-visible');
+  const token = JSON.parse(localStorage.getItem('token'));
+
+  // const url = `http://localhost:3000/api/v1/accounts/${accountNumber}`;
+  const url = `https://ile-ifowopamo.herokuapp.com/api/v1/accounts/${accountNumber}`;
+  console.log(url);
+
+  const init = {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'x-auth-token': token
+    },
+    method: 'DELETE'
+  };
+
+  fetch(url, init)
+    .then(response => response.json())
+    .then(response => {
+      if (response.status === 200) {
+        showMessage(response.message, 'success');
+        getAllAccounts();
+        yesButton.removeEventListener('click', deleteAccount);
+      }
+
+      yesButton.classList.remove('btn__loading-icon-visible');
+    })
+    .catch(error => console.log(error.message));
+};
+
+/* ******** activate or deactivate an account ******** */
+let newAccountStatus;
+
+const activateOrDeactivateAccount = () => {
+  const yesButton = document.querySelector('.btn-modal-yes');
+
+  yesButton.classList.add('btn__loading-icon-visible');
+  const token = JSON.parse(localStorage.getItem('token'));
+
+  // const url = `http://localhost:3000/api/v1/accounts/${accountNumber}`;
+  const url = `https://ile-ifowopamo.herokuapp.com/api/v1/accounts/${accountNumber}`;
+  console.log(url);
+  console.log(newAccountStatus);
+
+  const init = {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'x-auth-token': token
+    },
+    method: 'PATCH',
+    body: JSON.stringify({ accountStatus: newAccountStatus })
+  };
+
+  fetch(url, init)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+
+      if (response.status === 400) {
+        showMessage(response.error, 'failure');
+        getAllAccounts();
+        document.body.classList.add('modal--hidden');
+        yesButton.classList.remove('btn__loading-icon-visible');
+        yesButton.removeEventListener('click', activateOrDeactivateAccount);
+        return null;
+      }
+
+      if (response.status === 200) {
+        showMessage('Status changed successfully', 'success');
+        getAllAccounts();
+        document.body.classList.add('modal--hidden');
+        yesButton.classList.remove('btn__loading-icon-visible');
+        yesButton.removeEventListener('click', activateOrDeactivateAccount);
+        return null;
+      }
+    })
+    .catch(error => console.log(error.message));
+};
+
+/* ******** execute due action ******** */
+const executeAction = event => {
+  const yesButton = document.querySelector('.btn-modal-yes');
+  const modalMessageElem = document.querySelector('.modal-message');
+  const targetClasslist = event.target.classList;
+
+  if (
+    targetClasslist.contains('btn-table') ||
+    targetClasslist.contains('icon-tbl')
+  ) {
+    if (
+      targetClasslist.contains('btn-delete') ||
+      targetClasslist.contains('icon-delete')
+    ) {
+      setAccountNumber(event);
+      modalMessageElem.textContent = `Are you sure you want to delete this '${accountNumber}' account?`;
+      yesButton.addEventListener('click', deleteAccount);
     }
-  }  
-}
+
+    if (targetClasslist.contains('icon-activate')) {
+      setAccountNumber(event);
+      newAccountStatus = 'active';
+      modalMessageElem.textContent = `Are you sure you want to activate this '${accountNumber}' account?`;
+      yesButton.addEventListener('click', activateOrDeactivateAccount);
+    }
+
+    if (targetClasslist.contains('icon-deactivate')) {
+      setAccountNumber(event);
+      newAccountStatus = 'dormant';
+      modalMessageElem.textContent = `Are you sure you want to deactivate this '${accountNumber}' account?`;
+      yesButton.addEventListener('click', activateOrDeactivateAccount);
+    }
+  }
+};
 
 const accountsList = document.querySelector('.bank-accts-ls tbody');
-accountsList.addEventListener('click', getAccountNumberAndPerformAction);
+accountsList.addEventListener('click', executeAction);
