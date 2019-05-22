d852c518f8c1e3f6ddbb817fd6a8cd653444150c
Abdulfatai Jimoh <abdulfatai360@gmail.com>
2019-05-22 03:44:55 +0100

    feature(auth): log out user if authorization has expired
    [Delivers #166144674]

--- ui/js/validate-auth-token.js
+++ ui/js/validate-auth-token.js
@@ -1,28 +1,35 @@
 /* ******** check if authorization token exists/has expired ******** */
 const authToken = JSON.parse(localStorage.getItem('token'));
 const userRole = JSON.parse(localStorage.getItem('userRole'));
-const currPageUrl = window.location.pathname
+const currPageUrl = window.location.pathname;
 
 if (!authToken) window.location = '../signin.html';
 
-// const url = 'https://ile-ifowopamo.herokuapp.com/api/v1/accounts';
+const url = 'https://ile-ifowopamo.herokuapp.com/api/v1/tokens/expiration';
 // const url = 'http://localhost:3000/api/v1/accounts';
-// const init = {
-//   headers: {
-//     "Content-Type": "application/json; charset=utf-8",
-//     "x-auth-token": authToken
-//   }
-// };
+const myInit = {
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8'
+  },
+  body: JSON.stringify({ token: authToken }),
+  method: 'POST'
+};
 
-// fetch(url, init)
-//   .then(response => response.json())
-//   .then(response => {
-//     const { status } = response;
-//     if (status === 401) {
-//       window.location = '../signin.html';
-//     }
-//   });
+fetch(url, myInit)
+  .then(response => response.json())
+  .then(response => {
+    const { status } = response;
+    if (status === 400) {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      localStorage.removeItem('userRole');
 
+      window.location = '../signin.html';
+    }
+
+    return null;
+  })
+  .catch(error => console.log(error));
 
 if (currPageUrl.includes('/user') && !/^client$/i.test(userRole)) {
   window.location = '../signin.html';
@@ -35,5 +42,3 @@ if (currPageUrl.includes('/cashier') && !/^cashier$/i.test(userRole)) {
 if (currPageUrl.includes('/admin') && !/^admin$/i.test(userRole)) {
   window.location = '../signin.html';
 }
-
-
